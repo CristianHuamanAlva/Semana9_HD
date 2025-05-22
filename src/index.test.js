a96d7a5abe0032214b4c6578f01467e23c025d29
LAB-USR-LNORTE <LAB-USR-LNORTE@grupoutp.net>
2025-05-22 11:13:06 -0500

    Add another test

--- src/index.test.js
+++ src/index.test.js
@@ -3,3 +3,9 @@ const { add } = require('./index');
 test('add 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3);
 });
+
+test('adds 5 + 7 to equal 12', () => {
+  expect(add(5, 7)).toBe(12);
+});
+
+
